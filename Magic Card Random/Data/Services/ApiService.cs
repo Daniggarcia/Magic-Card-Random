d0458fdd9b8c90685a5c64890990fcf47bd2d28c
Daniel Gálvez García <daniel.gg@zoho.com>
2023-01-06 10:03:24 +0100

    feat(service): Api Service to call random card added

---
+++ Magic Card Random/Data/Services/ApiService.cs
@@ -0,0 +1,26 @@
+﻿using Magic_Card_Random.Data.Models;
+using Newtonsoft.Json;
+
+namespace Magic_Card_Random.Data.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<Card> GetRandomCard()
+        {
+            string url = "https://api.scryfall.com/cards/random";
+
+            using (var response = await _httpClient.GetAsync(url))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<Card>(apiResponse);
+            }
+        }
+    }
+}
