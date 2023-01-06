fede3d9fcc3dcbd7a8eb7214dd5cf62e374dc9b2
Daniel Gálvez García <daniel.gg@zoho.com>
2023-01-06 15:26:31 +0100

    feat(merge): API > Master

--- Magic Card Random/MainPage.xaml.cs
+++ Magic Card Random/MainPage.xaml.cs
@@ -1,24 +1,50 @@
-﻿namespace Magic_Card_Random;
+﻿using Magic_Card_Random.Data.Models;
+using Magic_Card_Random.Data.Services;
+
+namespace Magic_Card_Random;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private ApiService _apiService;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        _apiService = new ApiService();
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        imgCard.Source = null;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        Card card = await _apiService.GetRandomCard();
+        imgCard.Source = new UriImageSource
+        {
+            Uri = new Uri(card.image_uris.normal),
+            CachingEnabled = true,
+            CacheValidity = TimeSpan.FromDays(1)
+        };
+    }
+
+    private async void CounterBtn_Clicked(object sender, EventArgs e)
+    {
+        Card card = await _apiService.GetRandomCard();
+        imgCard.Source = new UriImageSource
+        {
+            Uri = new Uri(card.image_uris.normal),
+            CachingEnabled = true,
+            CacheValidity = TimeSpan.FromDays(1)
+        };
+        lblNameCard.Text = card.nameCard;
+        lblDescription.Text = card.flavor_text;
+    }
 }
 
