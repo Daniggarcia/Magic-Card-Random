2f7d1effabaa28a3a12c09dff2ac2dffb8fa2412
Daniel Gálvez García <daniel.gg@zoho.com>
2023-01-15 14:31:45 +0100

    feat(page): About Me Page added

--- Magic Card Random/Pages/AboutMe.xaml.cs
+++ Magic Card Random/Pages/AboutMe.xaml.cs
@@ -2,8 +2,44 @@ namespace Magic_Card_Random.Pages;
 
 public partial class AboutMe : ContentPage
 {
-	public AboutMe()
-	{
-		InitializeComponent();
-	}
+    public AboutMe()
+    {
+        InitializeComponent();
+    }
+
+    private async void SocialsImageButton_Clicked(object sender, EventArgs e)
+    {
+        ImageButton ImgBtn = (ImageButton)sender;
+        var socialImgBtn = ImgBtn.Source.ToString();
+        Uri uri = null;
+
+        switch (socialImgBtn)
+        {
+
+            case string a when a.Contains("linkedin"):
+                uri = new Uri("https://www.linkedin.com/in/daniel-galvez-garcia/");
+                break;
+            case string a when a.Contains("github"):
+                uri = new Uri("https://github.com/Daniggarcia");
+                break;
+            case string a when a.Contains("envelopesolid"):
+                string subject = "Hello DMan!!";
+                string body = "You're awesome DMan, I'm a Marton Stromgald fan too.";
+                string[] recipients = new[] { "daniel.gg@zoho.com" };
+
+                var message = new EmailMessage
+                {
+                    Subject = subject,
+                    Body = body,
+                    BodyFormat = EmailBodyFormat.PlainText,
+                    To = new List<string>(recipients)
+                };
+
+                await Email.Default.ComposeAsync(message);
+                break;
+
+
+        }
+        await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+    }
 }
